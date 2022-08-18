501b44fa66f87352ba7273ebe5364319eb86cc99
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-18 18:34:32 +0900

    名前・名前カナ欄における文字種別のバリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）を使用してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）を使用してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください" }
   validates :birthday, presence: true
 end
