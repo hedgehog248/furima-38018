237543d3f5883ad23e6b22520107e3ca233eb9da
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-19 00:48:47 +0900

    パスワードに英数字を含めるバリデーションを追加
    その他、DRYに沿って記述をまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_MESSAGE = "には英字と数字の両方を含めて設定してください"
+
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  NAME_MESSAGE = "には全角（漢字・ひらがな・カタカナ）を使用してください"
+
+  NAME_KANA_REGEX = /\A[ァ-ヶー]+\z/.freeze
+  NAME_KANA_MESSAGE = "には全角カタカナを使用してください"
+
+  validates :password, format: {with: PASSWORD_REGEX, message: PASSWORD_MESSAGE}
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）を使用してください" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）を使用してください" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください" }
+  validates :last_name, presence: true, format: { with: NAME_REGEX, message: NAME_MESSAGE }
+  validates :first_name, presence: true, format: { with: NAME_REGEX, message: NAME_MESSAGE }
+  validates :last_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: NAME_KANA_MESSAGE }
+  validates :first_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: NAME_KANA_MESSAGE }
   validates :birthday, presence: true
 end
