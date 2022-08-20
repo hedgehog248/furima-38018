f83305ecafea7eee122e27f9367f445c6fa6fe3e
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-20 17:50:34 +0900

    Rubocopによる修正を反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  PASSWORD_MESSAGE = "には英字と数字の両方を含めて設定してください"
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  PASSWORD_MESSAGE = 'には英字と数字の両方を含めて設定してください'.freeze
 
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
-  NAME_MESSAGE = "には全角（漢字・ひらがな・カタカナ）を使用してください"
+  NAME_MESSAGE = 'には全角（漢字・ひらがな・カタカナ）を使用してください'.freeze
 
   NAME_KANA_REGEX = /\A[ァ-ヶー]+\z/.freeze
-  NAME_KANA_MESSAGE = "には全角カタカナを使用してください"
+  NAME_KANA_MESSAGE = 'には全角カタカナを使用してください'.freeze
 
-  validates :password, format: {with: PASSWORD_REGEX, message: PASSWORD_MESSAGE}
+  validates :password, format: { with: PASSWORD_REGEX, message: PASSWORD_MESSAGE }
   validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: NAME_REGEX, message: NAME_MESSAGE }
   validates :first_name, presence: true, format: { with: NAME_REGEX, message: NAME_MESSAGE }
