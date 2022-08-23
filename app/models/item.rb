8d9310cef8ed7f8696e9b5e6f0429217788300c6
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-23 16:02:36 +0900

    発送までの日数(ActiveHash)の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee_payer
+  belongs_to :area
+  belongs_to :days_to_ship
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_fee_payer_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
 end
