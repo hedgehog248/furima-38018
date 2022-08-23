b88a70d458f8310028a21e0545e7b13a1354a671
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-23 19:45:47 +0900

    バリデーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,14 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :days_to_ship
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_fee_payer_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10000000, message: 'は300~9,999,999円の範囲で設定してください'},
+                    format: { with: /\A\d+\z/.freeze, message: 'は半角数字を使用してください'.freeze }, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :delivery_fee_payer_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
 end
