d600b16b07870b87d80952e04be4f24b34f6c67e
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-23 15:28:52 +0900

    配送料の負担欄（ActiveHash）の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,5 @@ class Item < ApplicationRecord
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_fee_payer_id, numericality: { other_than: 1, message: "cant be blank" }
 end
