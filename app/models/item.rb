bf1ef9164b562441954b91e4d40b850fd45e8d06
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-23 15:08:08 +0900

    商品の状態欄（ActiveHash）の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,5 @@ class Item < ApplicationRecord
   belongs_to :category
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
 end
