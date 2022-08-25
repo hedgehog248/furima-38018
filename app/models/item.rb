8d0d3f692cd22dc2e167b1506f20603e286e9b3f
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-08-25 23:24:16 +0900

    price欄のバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,8 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10000000, message: 'は300~9,999,999円の範囲で設定してください'},
-                    format: { with: /\A\d+\z/.freeze, message: 'は半角数字を使用してください'.freeze }, presence: true
+  validates :price, presence: true, 
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10000000 }
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
   validates :delivery_fee_payer_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
