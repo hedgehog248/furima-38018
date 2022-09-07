e6f46e0ab19108c8d49ff62c112bead4afc641bb
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-09-08 01:30:43 +0900

    PAY.JPの決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+  
   def index
-    @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.permit(:item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    binding.pry
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :item
   belongs_to :user
   has_one    :shipping_address
+
+  attr_accessor :token
+
+  validates :token, presence: true
 end
