03be1a6dfad94efca8a0488ed5038ca8f0be58e7
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-03-15 00:03:44 +0900

    ファイルがアップされるたびにイベント発火、画像の情報を変数に格納

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,4 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
   const previewList = document.getElementById('previews');
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
+
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  fileField.addEventListener('change', function (e) {
+    console.log("input要素で値の変化が起きました");
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+  });
 });
