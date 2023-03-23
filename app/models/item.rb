fc6f215862521543a0d34fd3b16f923ac9d8df4f
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-03-24 01:04:41 +0900

    itemモデルとitemsコントローラの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one    :order
-  has_one_attached :image
+  has_many_attached :images
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_fee_payer
   belongs_to :area
   belongs_to :days_to_ship
 
-  validates :image, presence: true
+  validates :images, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
   validates :price, presence: true,
